e8c69a62fcc8d5aa06fa8d0166e517ae810770f8
robomantis19 <freefavors@gmail.com>
2020-01-13 18:09:21 -0500

    added information to database met mvp

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ server.get('/', (req, res) => {
     res.send({hi: "Hello there from web25!!!"})
 })
 
-server.get('/api/hubs/', (req, res) => { 
+server.get('/api/users/', (req, res) => { 
     Hubs.find()
     .then(hubs => { 
         res.status(200).json(hubs); 
@@ -21,23 +21,98 @@ server.get('/api/hubs/', (req, res) => {
     .catch(error => { 
         console.log(error); 
         res.status(500).json({
-            errorMessage: 'sorry , we ran into an error getting the list on hubs.'
+            errorMessage: 'The user information could not be retrieved'
         })
     })
 })
 
-server.get('/api/hubs/:id', (req, res) => { 
+server.get('/api/users/:id', (req, res) => { 
     Hubs.findById(req.params.id)
+    
     .then(hubs => { 
         console.log('hubs by id', hubs); 
+        if(hubs === undefined){
+            res.status(404).json({
+                errorMessage: 'The user with the specified ID does not exist'
+            })
+        }
         res.status(200).json(hubs);
     })
     .catch(err => { 
-        console.log(err); 
         res.status(500).json({
-            errorMessage: 'sorry, error on get request by id'
+            errorMessage: 'The users information could not be retrieved'
+        })
+        
+    })
+})
+
+server.post('/api/users/', (req, res) => { 
+    const info = req.body
+    if(!info.name || !info.bio){
+        res.status(400).json({
+            errorMessage: "please provide your name and bio for the user"
+        })
+
+    }
+    Hubs.insert(info)
+    .then(hubs => { 
+        res.status(201).json(info)
+    })
+    .catch(err => { 
+        console.log(err); 
+        res.status(500).json(
+            {errorMessage: 'There was an error saving the user to the database '})
+    })
+})
+
+server.delete('/api/users/:id', (req, res) => { 
+    const ID = req.params.id;
+    
+    Hubs.remove(ID)
+    .then(hubs => {
+        console.log(hubs,"??delete")
+        if(hubs === 0) {
+             
+            res.status(404).json({
+                errorMessage: "The user with the specified ID does not exist."
+            })
+        }
+        res.status(200).json(hubs); 
+    })
+    .catch(error => { 
+        console.log(error); 
+        res.status(500).json({
+            errorMessage: 'The user could not be removed'
         })
     })
+})
+
+server.put('/api/users/:id', (req, res) => { 
+    // const ID = req.params.id; 
+    const body = req.body; 
+    const {id} = req.params;
+    if(!body.name || !body.bio){
+        res.status(400).json({
+            errorMessage: "please provide your name and bio for the user"
+        })
+
+    } 
+    Hubs.update(id, body)
+    .then(hubs =>  {
+        console.log(hubs, "??put")
+        if(hubs === 0){ 
+            res.status(404).json({
+                errorMessage: "The user with the specified user id does not exist"
+            }); 
+        }
+        res.status(200).json(hubs); 
+    })
+    .catch(error => { 
+        console.log(error); 
+        res.status(500).json({
+            errorMessage: "The user information could not be modified"
+        })
+    }) 
 })
 
 
